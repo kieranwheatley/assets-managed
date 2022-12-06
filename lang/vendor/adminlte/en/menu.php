898d8a0d125f9454fdf22133eefdad06ee73412a
kieranwheatley <kieran.wheatley@students.plymouth.ac.uk>
2022-12-06 13:04:03 +0000

    Added theme, displays users from database

---
+++ lang/vendor/adminlte/en/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'main_navigation'               => 'Main Navigation',
+    'blog'                          => 'Blog',
+    'pages'                         => 'Pages',
+    'account_settings'              => 'Account Settings',
+    'profile'                       => 'Profile',
+    'change_password'               => 'Change Password',
+    'multilevel'                    => 'Multi Level',
+    'level_one'                     => 'Level 1',
+    'level_two'                     => 'Level 2',
+    'level_three'                   => 'Level 3',
+    'labels'                        => 'LABELS',
+    'important'                     => 'Important',
+    'warning'                       => 'Warning',
+    'information'                   => 'Information',
+];
